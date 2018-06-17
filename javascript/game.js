40554e966b8d1fb610e3d0de804006b7a1a1531d
Barbara Egel <barbaraegel2012@u.northwestern.edu>
2018-06-17 16:23:55 -0500

    Sunday 4:23

---
+++ javascript/game.js
@@ -0,0 +1,41 @@
+
+
+//assign variables for the 19-120 number (targetNum), the number (1-12) for each crystal, wins, losses, and total score
+
+var targetNum = 0;
+var crystalNum = 0;
+var wins = 0;
+var losses = 0;
+var numSoFar = 0;
+
+//use JQ to code outputs for each
+
+$("#number-to-guess").text(targetNum);
+
+//onClick, start game
+
+//generate random targetNum
+
+for (var i = 0; i >18 && i < 121; i++) {
+    var random = Math.floor(Math.random()) + 1;);
+}
+
+//generate random crystalNum x4 (one for each crystal) -- use JQ .each function?
+// //$( document.body ).click(function() {
+//     $( "div" ).each(function( i )
+
+//document.log click value into numSoFar
+
+//repeat until numSoFar = targetNum then Wins +1
+
+//OR numSoFar > targetNum then Losses +1
+
+
+
+
+
+
+
+
+
+
