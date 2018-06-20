7a0d5766ec1650cefd9c0c66c03ed6ef43e15002
Barbara Egel <barbaraegel2012@u.northwestern.edu>
2018-06-20 18:33:05 -0500

    unit 4 game final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,7 @@
 
-
-//assign variables for the 19-120 number (targetNum), the number (1-12) for each jewel, wins, losses, and the jewel number as it accrues
-
 $(document).ready(function() {
+
+    //assign variables for the 19-120 number (targetNum), the number (1-12) for each jewel, wins, losses, and the jewel number as it accrues
 
         var numAccrue = 0;
         var targetNum = Math.floor(Math.random() * 101+19);
@@ -10,7 +9,7 @@ $(document).ready(function() {
         var losses = 0;
 
         //generate random targetNum
-        //generate random numAccrue
+        //generate random values for each jewel (I tried to do a foreach loop to write the jewel value assignments just once, but I made kind of a mess)
         $("#targetNum").text(targetNum);
             console.log(targetNum)
 
@@ -18,28 +17,35 @@ $(document).ready(function() {
         var pinkValue = Math.floor(Math.random()*11+1);
         var greenValue = Math.floor(Math.random()*11+1);
         var purpleValue = Math.floor(Math.random()*11+1);
-            console.log(aquaValue, " ", pinkValue, " ", greenValue, " ", purpleValue);
+            console.log(aquaValue, pinkValue, greenValue, purpleValue);
 
+        //Document wins and losses
             $("#losses").text("Losses: " + losses);
             $("#wins").text("Wins: " + wins);
     
-
+        //Finally figured out that when I want something complicated to happen, I should write it as its own function and then just call it. Duh.
         function win() {
-            alert("You won, smartypants.")
+            alert("You won, smartypants.");
             wins ++;
             $("#wins").text("Wins: " + wins);
+            //See below re: repeating the "Wins" string
             console.log("you won");
             resetGame();
         }
 
+        //Thing that ticks me off because I couldn't figure it out: numAccrue doesn't show the final matching or exceeding total (compared with targetNum)
+        //That is, if targetNum is 22, and numAccrue is 20 and you click on the crystal=2, numAccrue stays at 20 even though the "You win" alert happens.
+        //And then the reset seems to work okay.
+
         function lose() {
-            alert("Dang. Nice try.")
+            alert("Dang. Nice try.");
             losses ++;
             $("#losses").text("Losses: " + losses);
+            //Also, same problem as last week: recording a loss deletes (replaces?) the word "Losses," which is why I re-enter the "Losses" string here.
             console.log("you lost");
             resetGame();
         }
-
+        //Again, spent a lot of time trying to write the code that compares targetNum and numAccrue just once, but it worked better when I did it inside each click.
         $("#btn-aqua").on ("click", function() {
             numAccrue = numAccrue + aquaValue;
             console.log(numAccrue);
@@ -89,21 +95,19 @@ $(document).ready(function() {
                 }   
           }) 
 
-function resetGame () {
-    numAccrue = 0;    
-    $("#adderBox").text(numAccrue); 
-    targetNum = Math.floor(Math.random()*101+19);
-        $("#targetNum").text(targetNum);
-            console.log(targetNum);
-        aquaValue = Math.floor(Math.random()*11+1);
-        pinkValue= Math.floor(Math.random()*11+1);
-        greenValue = Math.floor(Math.random()*11+1);
-        purpleValue = Math.floor(Math.random()*11+1);
-        console.log(aquaValue, " ", pinkValue, " ", greenValue, " ", purpleValue);
-        
-        
-    };
-
-
+    function resetGame () {
+        numAccrue = 0;    
+        $("#adderBox").text(numAccrue); 
+        targetNum = Math.floor(Math.random()*101+19);
+            $("#targetNum").text(targetNum);
+                console.log(targetNum);
+            aquaValue = Math.floor(Math.random()*11+1);
+            pinkValue= Math.floor(Math.random()*11+1);
+            greenValue = Math.floor(Math.random()*11+1);
+            purpleValue = Math.floor(Math.random()*11+1);
+            console.log(aquaValue, pinkValue, greenValue, purpleValue);
+            
+            
+        };
 
 });
