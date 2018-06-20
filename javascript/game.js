b9902d4f27f93f2ea26684c95ae253257ff44bae
Barbara Egel <barbaraegel2012@u.northwestern.edu>
2018-06-20 11:33:54 -0500

    wednesday noon, mostly working

--- javascript/game.js
+++ javascript/game.js
@@ -1,41 +1,106 @@
 
 
-//assign variables for the 19-120 number (targetNum), the number (1-12) for each crystal, wins, losses, and total score
+//assign variables for the 19-120 number (targetNum), the number (1-12) for each jewel, wins, losses, and the jewel number as it accrues
 
-var targetNum = 0;
-var crystalNum = 0;
-var wins = 0;
-var losses = 0;
-var numSoFar = 0;
+$(document).ready(function() {
 
-//use JQ to code outputs for each
+        var numAccrue = 0;
+        var targetNum = Math.floor(Math.random() * 101+19);
+        var wins = 0;
+        var losses = 0;
 
-$("#number-to-guess").text(targetNum);
+        //generate random targetNum
+        //generate random numAccrue
+        $("#targetNum").text(targetNum);
+            console.log(targetNum)
 
-//onClick, start game
+        var aquaValue = Math.floor(Math.random()*11+1);
+        var pinkValue = Math.floor(Math.random()*11+1);
+        var greenValue = Math.floor(Math.random()*11+1);
+        var purpleValue = Math.floor(Math.random()*11+1);
+            console.log(aquaValue, " ", pinkValue, " ", greenValue, " ", purpleValue);
 
-//generate random targetNum
+            $("#losses").text(losses);
+            $("#wins").text(wins);
+    
 
-for (var i = 0; i >18 && i < 121; i++) {
-    var random = Math.floor(Math.random()) + 1;);
-}
+        function win() {
+            wins ++;
+            $("#wins").text(wins);
+            console.log("you won");
+            resetGame();
+        }
 
-//generate random crystalNum x4 (one for each crystal) -- use JQ .each function?
-// //$( document.body ).click(function() {
-//     $( "div" ).each(function( i )
+        function lose() {
+            losses ++;
+            $("#losses").text(losses);
+            console.log("you lost");
+            resetGame();
+        }
 
-//document.log click value into numSoFar
+        $("#btn-aqua").on ("click", function() {
+            numAccrue = numAccrue + aquaValue;
+            console.log(numAccrue);
+            $("#adderBox").text(numAccrue); 
+                if (numAccrue === targetNum){
+                  win();
+                }
+                else if (numAccrue > targetNum){
+                  lose();
+                }   
+          }) 
+          
+        $("#btn-pink").on ("click", function() {
+            numAccrue = numAccrue + pinkValue;
+            console.log(numAccrue);
+            $("#adderBox").text(numAccrue); 
+                if (numAccrue === targetNum){
+                    win();
+                }
+                else if (numAccrue > targetNum){
+                    lose();
+                }   
+          }) 
 
-//repeat until numSoFar = targetNum then Wins +1
+          
+        $("#btn-green").on ("click", function() {
+            numAccrue = numAccrue + greenValue;
+            console.log(numAccrue);
+            $("#adderBox").text(numAccrue); 
+                if (numAccrue === targetNum){
+                    win();
+                }
+                else if (numAccrue > targetNum){
+                    lose();
+                }   
+          }) 
 
-//OR numSoFar > targetNum then Losses +1
+          $("#btn-purple").on ("click", function() {
+            numAccrue = numAccrue + purpleValue;
+            console.log(numAccrue);
+            $("#adderBox").text(numAccrue); 
+                if (numAccrue === targetNum){
+                    win();
+                }
+                else if (numAccrue > targetNum){
+                    lose();
+                }   
+          }) 
+
+function resetGame () {
+    numAccrue = 0;    
+    targetNum = Math.floor(Math.random()*101+19);
+        $("#targetNum").text(targetNum);
+
+        aquaValue = Math.floor(Math.random()*11+1);
+        pinkValue= Math.floor(Math.random()*11+1);
+        greenValue = Math.floor(Math.random()*11+1);
+        purpleValue = Math.floor(Math.random()*11+1);
+        console.log(aquaValue, " ", pinkValue, " ", greenValue, " ", purpleValue);
+        
+        
+    };
 
 
 
-
-
-
-
-
-
-
+});
